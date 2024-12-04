6646a0dd84192b858cf8765bf86d20fe635280e5
reecewdoyle <144601273+reecewdoyle@users.noreply.github.com>
2024-12-04 22:39:45 +1100

    feat: added auth protection route

--- src/functions/jwtFunctions.js
+++ src/functions/jwtFunctions.js
@@ -18,13 +18,33 @@ function generateJWT(userId, username, roles = null){
     );
 }
 
-async function decodeJWT(tokenToDecode){
+function decodeJWT(tokenToDecode){
 
+    return jwt.verify(tokenToDecode, jwtSecretKey)
 }
 
 // middleware for validating auth 
 async function validateUserAuth(request, response, next){
+    let providedToken = request.headers.jwt;
+    console.log(providedToken);
 
+    if (!providedToken){
+        return response.status(403).json({
+            message: "Sign in to view this content!"
+        });
+           
+    }
+    
+    let decodedData = decodeJWT(providedToken);
+    console.log(decodedData);
+    if (decodedData.userId){
+        next();
+    } else {
+        return response.status(403).json({
+            message: "Sign in to view this content!"
+        });
+    }
+    
 }
 
 module.exports = {
--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@
 
 const express = require("express");
 const { User } = require("./models/UserModel");
-const { generateJWT } = require("./functions/jwtFunctions");
+const { generateJWT, validateUserAuth } = require("./functions/jwtFunctions");
 
 const app = express();
 
@@ -58,6 +58,12 @@ app.post("/signup", async (request, response) => {
 
 // // Export the app so that other files can control when the server
 // // starts and stops 
+app.get("/protectedRoute", validateUserAuth, (request, response) => {
+	response.json({
+		message:"You can see protected content because you're signed in!"
+	})
+})
+
 module.exports = {
 	app
 };
