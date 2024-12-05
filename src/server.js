410637919bfb975a8162deb9cb0fc90052c4ddd0
reecewdoyle <144601273+reecewdoyle@users.noreply.github.com>
2024-12-05 19:31:28 +1100

    feat: added CORS

--- src/server.js
+++ src/server.js
@@ -10,11 +10,19 @@
 const express = require("express");
 const { User } = require("./models/UserModel");
 const { generateJWT, validateUserAuth } = require("./functions/jwtFunctions");
+const cors = require ("cors");
 
 const app = express();
 
 app.use(express.json());
 // allows us to post JSON data to the server
+
+let corsOptions = {
+	//                 CRA local                Vite                Deployed React app
+	origin: ["http://localhost:8080", "http://localhost:5173", "https://vinylvibe.live"],
+	optionsSuccessStatus: 200
+};
+app.use (cors(corsOptions));
 
 // Server app configuration goes here
 // middleware, routes, etc 
