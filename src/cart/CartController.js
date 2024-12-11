846f690bbca64e55fe2bbc9c96c37dbec5fa29c5
reecewdoyle <144601273+reecewdoyle@users.noreply.github.com>
2024-12-11 16:49:23 +1100

    feat: added POST /cart to CartController.js

--- src/cart/CartController.js
+++ src/cart/CartController.js
@@ -29,8 +29,56 @@ const getCart = async (req, res, next) => {
     }
 };
 
+/**
+ * Add a new item to the cart
+ * POST /cart
+ */
+
+const addItem = async (req, res, next) => {
+    try {
+
+        // Extracts the 'userId' validated and attached to req.user by 'authMiddleware.js'
+        const userId = req.user._id;
+
+        // Destructures 'productId' and 'quantity' from the incoming request body
+        const { productId, quantity } = req.body;
+
+        // Ensures that 'productId' or 'quantity' are provided.
+        if (!productId || !quantity) {
+            
+            // Throw a 400 Bad Request error handled by errorMiddleware.js
+            return next(new AppError("Product ID and quantity are required", 400));
+        }
+
+        // See if the userId is already associadted with a cart document in the database
+        // If no cart exists, 'cart' is set to null.
+        let cart = await CartService.getCartByUserId(userId).catch(() => null);
+
+        // Add or update products in the cart
+        // - If 'cart' exists, update the cart
+        // - If 'cart' doesn't exist, create a new cart with the product
+        const updatedCart = cart
+            // Update existing cart
+            ? await CartService.addOrUpdateProducts(cart, [{ productId, quantity }])
+            // Creates a new cart 
+            : await CartService.createCart(userId, [{ productId, quantity }]);
+
+        // Sends a HTTP response staus code 201 when the request is 
+        // successfully created or updated
+        res.status(201).json({ 
+            status: "success", 
+            data: updatedCart,
+        });
+
+    // Foreard any errors to the errorHandler middleware
+    } catch (error) {
+        next(error);
+    }
+};
+
 
 
 module.exports = {
     getCart,
+    addItem,
 }
