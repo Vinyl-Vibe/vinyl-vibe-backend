a781b7c2aaf692e8331952d3b0614921558a86cd
reecewdoyle <144601273+reecewdoyle@users.noreply.github.com>
2024-12-12 00:45:55 +1100

    fix: updated files from main branch

--- src/users/UserService.js
+++ src/users/UserService.js
@@ -10,13 +10,13 @@ const { AppError } = require('../utils/middleware/errorMiddleware');
  * - Easier to test business logic
  */
 const UserService = {
-	async createUser({ email, password, role }) {
+	async createUser({ email, password }) {
 		try {
 			const existingUser = await User.findOne({ email });
 			if (existingUser) {
 				throw new AppError('User already exists', 400);
 			}
-			return await User.create({ email, password, role });
+			return await User.create({ email, password });
 		} catch (error) {
 			if (error.isOperational) throw error;
 			throw new AppError('Error creating user', 500);
