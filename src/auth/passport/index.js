e3ad7d3369ac971909e3bca165261192658cc3ab
Damian Petrov <hello@damianpetrov.com>
2024-12-13 01:43:15 +1100

    fix: incorrect client key from apple console

--- src/auth/passport/index.js
+++ src/auth/passport/index.js
@@ -1,38 +1,13 @@
-const passport = require("passport");
-const { User } = require("../../users/UserModel");
+const passport = require('passport');
 
-/**
- * Passport configuration
- *
- * Why use Passport?
- * - Industry standard for OAuth
- * - Supports multiple providers
- * - Handles complex OAuth flows
- * - Extensive middleware ecosystem
- */
-
-// Serialize user for the session
-// Why serialize?
-// - Convert user object to simple identifier
-// - Minimize session storage
-// - Improve performance
-passport.serializeUser((user, done) => {
-    done(null, user._id);
+passport.serializeUser(function(user, cb) {
+    cb(null, user);
 });
 
-// Deserialize user from the session
-// Why deserialize?
-// - Convert identifier back to user object
-// - Fetch fresh user data
-// - Ensure up-to-date user info
-passport.deserializeUser((id, done) => {
-    User.findById(id)
-        .then(user => done(null, user))
-        .catch(err => done(err));
+passport.deserializeUser(function(obj, cb) {
+    cb(null, obj);
 });
 
-// Import strategies
-require("./googleStrategy");
-require("./appleStrategy");
+require('./appleStrategy');
 
 module.exports = passport;
